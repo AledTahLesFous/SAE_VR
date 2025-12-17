00e2e52e202b9f3aabdeabaed7e48119852e1544
Atom <clement.muzard@etu.umontpellier.fr>
2025-12-17 16:13:34 +0100

    merge vr branch for test

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,7 @@ public class SceneLoader : MonoBehaviour
 {
     void Start()
     {
+        SceneManager.LoadScene("01 - Player", LoadSceneMode.Additive);
         SceneManager.LoadScene("02 - Space", LoadSceneMode.Additive);
         SceneManager.LoadScene("02 - Key", LoadSceneMode.Additive);
     }
